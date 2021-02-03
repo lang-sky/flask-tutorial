6fef49d1171c0618782e74e2742f3f1562ddbc9a
lang-sky <langsky121@gmail.com>
2021-02-03 16:56:59 -0500

    part2 - templates

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+### followed
+
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
+### how to run
+
+```
+$ source venv/bin/activate
+$ flask run
+```
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!',
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!',
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+#     return '''
+# <html>
+#     <head>
+#         <title>Home Page - Microblog</title>
+#     </head>
+#     <body>
+#         <h1>Hello, ''' + user['username'] + '''!</h1>
+#     </body>
+# </html>'''
